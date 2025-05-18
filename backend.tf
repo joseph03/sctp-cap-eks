8f4c801aadad7fcb7bde1e1b81adc06373d95ba2
joseph03 <joseph03sg@gmail.com>
2025-05-18 20:24:13 +0800

    test16

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   # variable is not allowed in backend block
   backend "s3" {
-    bucket         = "grp-3joseph03-cap-eks-tfstate"
+    bucket         = "grp-3joseph03-s3"
     key            = "cap-eks/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "grp-3joseph03-cap-eks-tf-locks"
+    dynamodb_table = "grp-3joseph03-dynamodb"
     encrypt        = true
   }
 }
